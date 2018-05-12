de7c445b230ef22224ae9388113cb1f359e0c941
Sebastian <sebasortuno@gmail.com>
2018-05-11 20:26:08 -0400

    firts commit

--- src/bo/usfx/sis457/POOFigurasGeometricas.java
+++ src/bo/usfx/sis457/POOFigurasGeometricas.java
@@ -29,6 +29,9 @@ public class POOFigurasGeometricas {
         System.out.println("| 1) Calcular el area y perimetro de un Circulo    |");
         System.out.println("| 2) Calcular el area y perimetro de un Cuadrado   |");
         System.out.println("| 3) Calcular el area y perimetro de un Rectangulo |");
+        System.out.println("| 4) Calcular el area y perimetro de un TriaguloEq |");
+        System.out.println("| 5) Calcular el area y perimetro de un Pentagono  |");
+        System.out.println("| 6) Calcular el area y perimetro de un Rombo      |");
         System.out.println("| *) Salir                                         |");
         System.out.println("+==================================================+");
         entradaTeclado = new Scanner(System.in);
@@ -76,6 +79,52 @@ public class POOFigurasGeometricas {
                 System.out.println("El Area del Rectangulo es: " + rectangulo.getArea());
                 System.out.println("El Perimetro del Rectangulo es: " + rectangulo.getPerimetro());
                 break;
+                 case 4:
+                /* Se instancia un objeto de la Clase Triangulo y posteriormente se modifica el valor
+                   de su propiedad "Lado" a través de un método accesor o setter (setLado). */
+                Triangulo triangulo = new Triangulo();
+                limpiarConsola();
+                System.out.println("+==================================================+");
+                System.out.println("| SubPrograma: Triangulo                           |");
+                System.out.println("+==================================================+");
+                System.out.print("Introduzca el valor del lado de un triangulo equilatero: ");
+                float lado = (float)entradaTeclado.nextFloat();
+                triangulo.setLado(lado);
+                System.out.println("El Area del Triangulo Equilatero es: " + triangulo.getArea());
+                System.out.println("El Perimetro del Triangulo Equilatero es: " + triangulo.getPerimetro());
+                break;    
+            case 5:
+                /* Se instancia un objeto de la Clase Rectangulo y a la vez se inicilaizan los valores
+                   de sus propiedades "Base" y "Altura" a través del método Constructor (Rectangulo). */
+                limpiarConsola();
+                System.out.println("+==================================================+");
+                System.out.println("| SubPrograma: Pentagono                           |");
+                System.out.println("+==================================================+");
+                System.out.print("Introduzca el valor del Lado: ");
+                double ladoP =  entradaTeclado.nextDouble();
+                System.out.print("Introduzca el valor del apotema: ");
+                double apotema = entradaTeclado.nextDouble();
+                Pentagono pentagono = new Pentagono(ladoP, apotema);
+                System.out.println("El Area del Pentagono es: " + pentagono.getArea());
+                System.out.println("El Perimetro del Pentagono es: " + pentagono.getPerimetro());
+                break;
+            case 6:
+                /* Se instancia un objeto de la Clase Rectangulo y a la vez se inicilaizan los valores
+                   de sus propiedades "Base" y "Altura" a través del método Constructor (Rectangulo). */
+                limpiarConsola();
+                System.out.println("+==================================================+");
+                System.out.println("| SubPrograma: Rombo                               |");
+                System.out.println("+==================================================+");
+                System.out.print("Introduzca el valor del DiagonalMayor: ");
+                double diagonalMayor =  entradaTeclado.nextDouble();
+                System.out.print("Introduzca el valor del DiagonalMenor: ");
+                double diagonalMenor = entradaTeclado.nextDouble();
+                System.out.print("Introduzca el valor del Lado: ");
+                double ladoR = entradaTeclado.nextDouble();
+                Rombo rombo = new Rombo(diagonalMayor, diagonalMenor,ladoR);
+                System.out.println("El Area del Rombo es: " + rombo.getArea());
+                System.out.println("El Perimetro del Rombo es: " + rombo.getPerimetro());
+                break;      
             default:
                 System.out.println("Salio del programa");
         }
